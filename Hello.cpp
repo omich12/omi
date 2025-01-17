d2098c4347c1d5984add6c9342319fcaefbfa7db
omich12 <omichau06@gmail.com>
2025-01-17 10:42:39 +0545

    first commit

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hello World" << std::endl;
+//     return 0;
+// }
+
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+    cout << "Hello World123456" << endl;
+    cout << "BYE";
+    return 0;
+}
